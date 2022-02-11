9f080de4f58e63cbae27796f825587ce53fa81f2
vikrantdotnet2021 <95204513+vikrantdotnet2021@users.noreply.github.com>
2022-02-12 00:36:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// wap switch
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        public static void Main()
+        {
+            //if else 
+            //wap to accept 2 no. and operator
+            int n1, n2, res = 0;
+            char op;
+            Console.WriteLine("Enter 2 no");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operator");
+            op = Convert.ToChar(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    res = n1 + n2;
+                    break;
+                case '-':
+                    res = n1 - n2;
+                    break;
+                case '*':
+                    res = n1 * n2;
+                    break;
+                default:
+                    Console.WriteLine("invalid");
+                    break;
+
+            }
+
+            Console.WriteLine("res " + res);
+            Console.ReadLine();
+        }
+    }
+}
+
