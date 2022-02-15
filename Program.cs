395ef82403da96686b37130f2af0074f711674d1
vikrantdotnet2021 <95204513+vikrantdotnet2021@users.noreply.github.com>
2022-02-16 00:14:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// wap datatype in csharp  byte and sbyte
+﻿// wap float
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +12,12 @@ namespace ConsoleApp1
 
         public static void Main()
         {
-            byte b = 25; // unsigned byte for storing positive no.
-            Console.WriteLine("byte value " + b);
-            Console.WriteLine("enter byte");
-            b = Convert.ToByte(Console.ReadLine());
-            Console.WriteLine("byte = " + b);
-            //for negative as well as positive no.
-            sbyte b1 = -23; //signed byte
-            Console.WriteLine("sbyte value " + b1);
-            Console.WriteLine("enter sbyte");
-            b1 = Convert.ToSByte(Console.ReadLine());
-            Console.WriteLine("sbyte = " + b1);
+            float b = 123.4567890f; //signed
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("value = " + b);
+
 
 
             Console.ReadLine();
